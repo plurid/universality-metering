fd42d837416f5879c3ccc868aa15a86098b0bcde
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-12-21 13:10:23 +0200

    dev: text selectors

--- src/ts/source/components/universality-metering/universality-metering.ts
+++ src/ts/source/components/universality-metering/universality-metering.ts
@@ -16,10 +16,18 @@ class UniversalityMetering extends LitElement {
         return this;
     }
 
+    toggle() {
+        console.log('toggle')
+    }
+
     render() {
         return html`
-            <div class="universality-metering-selector">
-                UM
+            <div class="universality-metering-selectors">
+                <div class="universality-metering-selector universality-metering-selector-um" @click=${ (e: Event) => this.toggle() }>UM</div>
+                <div class="universality-metering-selector">Text</div>
+                <div class="universality-metering-selector">Paragraph</div>
+                <div class="universality-metering-selector">Sentence</div>
+                <div class="universality-metering-selector">Word</div>
             </div>
         `;
     }
