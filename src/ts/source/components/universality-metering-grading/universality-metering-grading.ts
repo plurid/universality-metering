3d456fde7ecca37771b0a45b5859ec8a855e8cba
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-12-22 14:39:10 +0200

    dev: minimal grading ui

--- src/ts/source/components/universality-metering-grading/universality-metering-grading.ts
+++ src/ts/source/components/universality-metering-grading/universality-metering-grading.ts
@@ -31,7 +31,69 @@ class UniversalityMeteringGrading extends LitElement {
     render() {
         return html`
             <div class="universality-metering-grading">
-                ${capitalize(this.mode)} Universality
+                <div class="universality-metering-grading-header">
+                    <div class="universality-metering-grading-header-numbers">
+                        <div class="universality-metering-grading-header-switch-group">
+                            <div>
+                                Crisp
+                            </div>
+
+                            <div>
+                                <label class="universality-metering-grading-header-switch">
+                                    <input type="checkbox" checked>
+                                    <span class="universality-metering-grading-header-slider universality-metering-grading-header-slider-round"></span>
+                                </label>
+                            </div>
+
+                            <div>
+                                Fuzzy
+                            </div>
+                        </div>
+                    </div>
+                    <div class="universality-metering-grading-header-type">
+                        <div class="universality-metering-grading-header-switch-group">
+                            <div>
+                                Type 1
+                            </div>
+
+                            <div>
+                                <label class="universality-metering-grading-header-switch">
+                                    <input type="checkbox" checked>
+                                    <span class="universality-metering-grading-header-slider universality-metering-grading-header-slider-round"></span>
+                                </label>
+                            </div>
+
+                            <div>
+                                Type 2
+                            </div>
+                        </div>
+                    </div>
+                    <div class="universality-metering-grading-header-pluridimensional">
+                        <input type="checkbox">
+                        <label>Pluridimensional</label>
+                    </div>
+
+                    <div class="universality-metering-grading-header-kind">
+                        ${capitalize(this.mode)} Universality
+                    </div>
+                </div>
+                <div class="universality-metering-grading-area">
+                    <input type="range" min="-1000" max="1000">
+                </div>
+                <div class="universality-metering-grading-buttons">
+                    <button class="universality-metering-grading-button">
+                        Mine
+                    </button>
+                    <button class="universality-metering-grading-button">
+                        Median
+                    </button>
+                    <button class="universality-metering-grading-button">
+                        Machine
+                    </button>
+                    <button class="universality-metering-grading-button">
+                        Friends
+                    </button>
+                </div>
             </div>
         `;
     }
