8561c21eae3a930a0ff20a63d3583e8d98abf580
Jan Cavel <caveljan@gmail.com>
2018-12-22 13:01:03 +0200

    dev: paragraph selector

--- src/ts/source/components/universality-metering-p/universality-metering-p.ts
+++ src/ts/source/components/universality-metering-p/universality-metering-p.ts
@@ -1,5 +1,5 @@
 import { LitElement, html } from '@polymer/lit-element';
-
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 
 
 class UniversalityMeteringP extends LitElement {
@@ -16,9 +16,31 @@ class UniversalityMeteringP extends LitElement {
         return this;
     }
 
+    firstUpdated() {
+        this.showGrading();
+    }
+
+    showGrading() {
+        const p: HTMLElement = this.querySelector('p');
+        const grading: HTMLElement = this.querySelector('universality-metering-grading');
+
+        p.addEventListener('mouseenter', () => {
+            p.classList.add('p-hover-universality-metering');
+            grading.style.display = 'block';
+        });
+
+        p.addEventListener('mouseleave', () => {
+            p.classList.remove('p-hover-universality-metering');
+            grading.style.display = 'none';
+        });
+
+        console.log(p);
+        console.log(grading);
+    }
+
     render() {
         return html`
-            ${this.innerHTML}
+            ${ unsafeHTML(this.innerHTML) }
 
             <universality-metering-grading mode="paragraph">
             </universality-metering-grading>
